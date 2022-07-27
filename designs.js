c3e7fa4b291999dc5ea84cb35b87cc3722a45b2d
Joshua Nelson <jmn2020@hotmail.com>
2022-07-27 18:12:29 -0400

    new

--- designs.js
+++ designs.js
@@ -4,7 +4,27 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
+    document.querySelector('h2').classList.add('.rainbow-td');
 // Your code goes here!
+    var Table = document.getElementById("pixelCanvas");
+    Table.innerHTML = "";
 
+    let hdight =  document.getElementById('inputHeight').value;
+    let width = document.getElementById("inputWidth").value;
+    // let submit = document.querySelector('input[type=submit]');
+    let colorChoice = document.getElementById("colorPicker").value;
+
+    let tableGrid = document.querySelector('table');
+    for (let j = 0; j < hdight; j++) {
+        let tr = document.createElement('tr');
+        
+        for (let i = 0; i < width; i++) {
+            let newEl = document.createElement('td');
+            newEl.addEventListener('click', function turnBlue(element) {
+                newEl.style.background = colorChoice;
+            });
+            tr.appendChild(newEl);
+        }
+        tableGrid.appendChild(tr);
+    }
 }
