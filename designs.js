b4dad62730d8077cbc9c8072725f80df53b4eada
Joshua Nelson <jmn2020@hotmail.com>
2022-07-29 00:16:59 -0400

    round two

--- designs.js
+++ designs.js
@@ -1,30 +1,34 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
+//Get the table to fill with the grid
+const tableGrid = document.querySelector('table');
+ // Select size input
+const inputHdight =  document.getElementById('inputHeight');
+const inputWidth = document.getElementById("inputWidth");
 
 function makeGrid() {
-    document.querySelector('h2').classList.add('.rainbow-td');
 // Your code goes here!
-    var Table = document.getElementById("pixelCanvas");
-    Table.innerHTML = "";
+    //Clear the table every time the submit button is pushed
+    let table = tableGrid;
+    let height = inputHdight.value;
+    let width = inputWidth.value;
 
-    let hdight =  document.getElementById('inputHeight').value;
-    let width = document.getElementById("inputWidth").value;
-    // let submit = document.querySelector('input[type=submit]');
-    let colorChoice = document.getElementById("colorPicker").value;
-
-    let tableGrid = document.querySelector('table');
-    for (let j = 0; j < hdight; j++) {
+    table.innerHTML = "";
+    
+    for (let j = 0; j < height; j++) {
         let tr = document.createElement('tr');
-        
+        //The table row is fo the height of the grid
         for (let i = 0; i < width; i++) {
+            //A nested loop to fill each row wioth the user selected width
             let newEl = document.createElement('td');
+            //Event listener to change the color when user clicks a square
             newEl.addEventListener('click', function turnBlue(element) {
-                newEl.style.background = colorChoice;
+                // Select color input
+                newEl.style.background = document.getElementById("colorPicker").value;
             });
+            //add each new grid to the parent row
             tr.appendChild(newEl);
         }
-        tableGrid.appendChild(tr);
+        //add each row to the parent table
+        table.appendChild(tr);
     }
 }
